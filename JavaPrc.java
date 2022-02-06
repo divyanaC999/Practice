5747bc00f5f33872c7258c4ff55597d8627acccf
divyanaC999 <divyachourasiya999@gmail.com>
2022-02-06 16:48:43 +0530

    Added: addmy file

---
+++ JavaPrc.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+public class JavaPrc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+		
+	
+	}
+
+}
